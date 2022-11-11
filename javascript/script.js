a65e4466e3f42d57d948c186bd0ce9bbeba36154
Hernan1Escudero <hernanescudero2@gmail.com>
2022-11-11 03:52:38 -0300

    "commit5"

--- javascript/script.js
+++ javascript/script.js
@@ -3,72 +3,46 @@
 let allNotes = ["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 
 //consulta result funciona como contenedor de la respuesta y como variable de control para hacer validaciones conta allNotes
-let consultaResult = [];
+let consultaResult = "";
 
 // truth es un tambien valor de control para contar la cantidad de notas no repetidas
 let truth = 0;
 //lie valida cuantas notas o valores repetidos que hay 
 let lie = 0;
-debugger;
-//saludo inicial para poder saber si ejecutar los pasos o cancelar el total
-let welcome= ()=> {
-    let confirmation =confirm("Presione un boton 'aceptar' para proseguir, o 'cancelar' para no hacer  nada");
-    return confirmation;
-  }
+
 
 // Obtiene las notas en cifrado americano, osea en letras mayusculas(toUpperCase) y las mete dentro del array de comprobacion 
-let notes=( )=>{
-   
-   
-      for(let i=0; i< 7 ; i++){
-        let consulta = prompt("Ingrese las notas para saber si es una escala en cifrado americano").toUpperCase(); 
-    
-        //Agrega los valores a la variable de control
-      let pusher = consultaResult.push(consulta);
-      
-      }
-      
-    
-  
-  }
 
+let obtenerDatosIngreso = document.getElementById('exampleInputEmail1');
+obtenerDatosIngreso.addEventListener("change",(e)=>{
 
+  e.preventDefault()
+  let notes = document.getElementById("exampleInputEmail1")
+  consultaResult = notes.value.toUpperCase().split('');
 
-// let notesQuantity =()=>{
-//     let index = consultaResult.length;
-//     console.log(consultaResult+" " +index);
+ });
 
-// };
-
- // count , hace la cuenta de cuantos valores del array de notas(counsultaResult) esta dentro de la escala cromatica(allNotes) , la cual contiene todas las notas posibles en el sistema occidental
- let countRepetitions =()=>{ 
-    
+ // count , hace la cuenta de cuantos valores del array de notas(counsultaResult) esta dentro de
+ //la escala cromatica(allNotes) , la cual contiene todas las notas posibles en el sistema occidental
+ let countRepetitions =()=>{  
     for(let i=0; i<= (allNotes.length)-1;i++)
     {
-
-  if(allNotes.indexOf(consultaResult[i]) > -1){
+   if(allNotes.includes(consultaResult[i])){
     //console.log("contiene la nota")
-     truth +1
+      truth +=1
     }
-     //INTENTO FALLIDO
-    // if(allNotes.includes(consultaResult[i])){
-    //   alert("No se pueden ingresar letras repetidas")
-    //   break;
 
-    // };
-
-   //oh  
-  
-    else if (allNotes.indexOf(consultaResult[i]) == -1){
+    else {
 
       //console.log("no contiene la nota")
-      lie +1;
+       lie +=1;
 
-      
     } 
 
 }
+
 };
+
 
  //Lo que quiero con esta funcion final es que, las funciones solo se ejecuten cuando yo las llame
 let notesMessage=()=>{
@@ -76,32 +50,79 @@ let notesMessage=()=>{
   let notScale ="no ha ingresado una escala";
   let isScale = "lo ingresado coincide con una escala musical";
    
-  //Defino el iniciador/cancelador
-  if( welcome() == false){ 
-    alert("A decidido no ingresar valores.Que tenga un buen día")
-    console.log(notScale);
-    }
-     
-      else{ 
+  
   // en caso de confirmar se ejecutan las funciones ya explicadas
- notes();
+  
+   
  
  countRepetitions()
   //Al final se hacen las validaciones en base a una variable que me dira si se ingresa o no una escala en base a la cantidad de valores 
  //Una escala musical minimamente consta de 5 notas, en caso de ingresar menos de 4(por ejemplo se ingresaron numeros o palabras,pero las letras fueron menores a 5 la respuesta va a ser no hay escala)
-if (truth<5 && lie>=5){
+ let alert = document.getElementById("forAlert");
+ let alertBarDanger = alert.innerHTML = '<div class="alert alert-danger" role="alert">'+notScale+'</div>';
+ if(consultaResult.length== 0){
+  alert.innerHTML = alertBarDanger
+  truth = 0;
+   lie = 0;
+ }
+
+ else if (truth<5 ){
    console.log(notScale);
-   alert(notScale);
+   alert.innerHTML = alertBarDanger
+   truth = 0;
+   lie = 0;
 }
-else{
+else if (truth >=5){
   console.log( isScale);
-  alert( isScale);
-}
-      
-}
+  
+alert.innerHTML = '<div class="alert alert-primary" role="alert">'+isScale+'</div>'; 
+truth = 0;
+lie = 0;
+}   
+
 };
 
+let comprobar = document.getElementById('btn');
+comprobar.addEventListener("click",(e)=>{
 
-notesMessage()
+ e.preventDefault()
+ 
+ notesMessage();
 
+
+});
+
+let coca =document.getElementById("flexSwitchCheckChecked");
+coca.setAttribute('value','unchecked')
+
+
+coca.addEventListener("click",()=>{
+  if (coca.value == 'unchecked'){
+    document.body.classList.add("darkMode")
+    coca.removeAttribute('value','unchecked')
+    coca.setAttribute('value','checked')}
+    else{
+
+      document.body.classList.remove("darkMode")
+      coca.removeAttribute('value','checked')
+    coca.setAttribute('value','unchecked')}
+    }
+
+
+
+)
+
+
+
+
+
+
+
+
+
+//butt.addEventListener("click",saludo)
+
+///////////////////////////////////////////////
+
+// localStorage.setItem("cliente","pepe")
 
